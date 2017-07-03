0e3480e88e0ef5c3012711121bd761d2aad63ae1
Alessandro Mangiameli <alessandro.mangiameli@gmail.com>
2017-07-03 08:23:21 +0200

    refactor folder structure

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import BoardContainer from 'containers/BoardContainer';
+
+const App = () => (
+    <div>
+        <h1>test</h1>
+        <BoardContainer />
+    </div>
+);
+
+export default App;
